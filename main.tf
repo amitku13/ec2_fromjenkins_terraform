5552cccc8da935ac7cef19a5d582cb4c076e7ab7
amitku13 <amitkumarbhontele@gmail.com>
2024-10-26 06:25:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,37 @@
 provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_instance" "example" {
-  ami           = "ami-06b21ccaeff8cd686" # Replace with your preferred AMI ID
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "Terraform-EC2"
-  }
-}
-
+ region = "ap-south-1"
+ }
+ variable "ami_id" {
+ description = "AMI ID to use for the EC2 instance"
+ default
+ = "ami-06b21ccaeff8cd686" // Default AMI ID
+ }
+ variable "instance_tag_name" {
+ description = "Name tag for the EC2 instance"
+ default
+ = "terraform" // Default instance tag name
+ }
+ variable "subnet_id" {
+ description = "Subnet ID where the EC2 instance will be launched"
+ default
+ = "subnet-09dc057c84389ffcb" // Default subnet ID
+ }
+ variable "vpc_id" {
+ description = "VPC ID where the EC2 instance will be launched"
+ default
+ = "vpc-006993ee517a74e91" // Default VPC ID
+ }
+variable "vpc_cidr" {
+ description = "CIDR block for the VPC"
+ default
+ = "172.31.0.0/16" // Default VPC CIDR
+ }
+ resource "aws_instance" "example" {
+ ami
+ = var.ami_id
+ instance_type = "t2.micro"
+ subnet_id = var.subnet_id
+ tags = {
+ Name =var.instance_tag_name
+ }
+ 
